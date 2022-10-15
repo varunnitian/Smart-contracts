9fd623d47474176ef6bcb71e3517acea0224abbc
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-10-15 23:03:15 +0530

    Add files via upload

---
+++ 1_identity.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+contract Identity
+{
+    string name;
+    uint age;
+    constructor() public
+    {
+name="varun";
+age=17;
+    }
+    function getName() view public returns(string memory){
+        return name;
+    }
+    function getAge() view public returns(uint){
+        return age;
+    }
+    function setAge() public 
+    {
+        age=age+1;
+    }
+}
